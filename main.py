f4e01fe41a7bdea15248fe234cc89a1cc6c73b9c
lior.benzvi <lior.benzvi@start.io>
2022-05-07 12:15:11 +0300

    fix: stupid things

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import sys
 import pandas as pd
 import numpy as np
+import random
 
 ratings_file_name = "yelp_data/Yelp_ratings.csv"
 
@@ -36,11 +38,36 @@ def accuracy(predictions, targets):
     return correct / total
 
 
+# Q4
+def train_base_model(k, ratings_train_df, gamma, lambda_parm):
+    train, validate = \
+        np.split(ratings_train_df.sample(frac=1, random_state=42),
+                 [int(.8 * len(ratings_train_df))])
+
+    m = train['stars'].mean()
+    bu, bi, pu, qi = random.choice((-1, 1)) * 0.00005
+
+    for rui in train['stars']:
+        eui = rui - m - bi - bu - pu - qi
+        bu = bu + gamma * (eui - lambda_parm * bu)
+        bi = bi + gamma * (eui - lambda_parm * bi)
+        qi = qi + gamma * (eui * pu - lambda_parm * qi)
+        pu = pu + gamma * (eui * qi - lambda_parm * pu)
+        res = qi * pu
+
+    ## where do I use K ?
+    ### ????
+    rmse_old = sys.maxsize
+    rmse_new = rmse(y_pred, validate['stars'])
+    if (rmse_new < rmse_old):
+
+
+
 if __name__ == '__main__':
     # business_df = pd.read_csv("yelp_data/yelp_business.csv", encoding="UTF-8")
     # ratings_df = pd.read_csv("yelp_data/Yelp_ratings.csv", encoding="UTF-8")
     # ratings_demo_df = pd.read_csv("yelp_data/Yelp_ratings_DEMO.csv", encoding="UTF-8")
     ratings_test_df, ratings_train_df = test_train_split()
+    gamma, lambda_parm = 0.05, 0.05
+    train_base_model(100, ratings_train_df, gamma, lambda_parm)
     print(ratings_test_df.head())
-    print(ratings_train_df.head())
-
