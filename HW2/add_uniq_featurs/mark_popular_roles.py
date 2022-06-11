6f69995fd3a61ff2fd8994596f8b566523fba7ab
lior.benzvi <lior.benzvi@start.io>
2022-06-11 20:56:28 +0300

    fix: files

--- HW2/add_uniq_featurs/mark_popular_roles.py
+++ HW2/add_uniq_featurs/mark_popular_roles.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
         if choice_value == 1:
             final_dict[role] = final_dict.get(role, 0) + 1
 
-    roles_data = pd.read_csv("../csv_files/roles_data/additional_data_on_roles.csv", encoding="UTF-8")
+    roles_data = pd.read_csv("../csv_files/roles_data/full_roles_data.csv", encoding="UTF-8")
     roles_data['is_popular'] = roles_data.apply(lambda r: is_popular(r), axis=1)
     print('Total {0} popular roles'.format(str(roles_data['is_popular'].sum())))
     roles_data.to_csv('../csv_files/roles_data/full_roles_data.csv', encoding="utf-8")
